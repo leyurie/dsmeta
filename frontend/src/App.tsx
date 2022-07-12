5b3cca319062cf8fd5174df8a4d9123b89169380
leyurie <leticia.yu77@gmail.com>
2022-07-12 05:55:44 -0400

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
+  return (
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton />
+    <NotificationButton />
+    </>
   )
 }
 
